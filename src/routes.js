4a989827709a6a40ee1d2f242fabb67f7aa1aeb8
Nikolo Iosifovich <vieiraneto88@gmail.com>
2021-08-27 16:23:31 -0300

    feat: ensure middleware for authorized user token

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,20 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
+require('dotenv').config()
+
+function checkAValidUserToken(req, res, next) {
+  const {usertoken} = req.headers
+
+  if(usertoken !== process.env.TOKEN){
+    return res.status(404).json({
+      error: 'Unautorized!'
+    })
+  }
+
+  return next()
+}
+
+router.get('/', checkAValidUserToken, (req, res) => {
   return res.json({
     message: 'Done!'
   })
